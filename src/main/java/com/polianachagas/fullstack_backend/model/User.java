fa91128f67c37530ad41f4ef6d7b8dcc57ea7a58
Poliana Chagas <polianarc934@gmail.com>
2024-08-04 22:02:06 -0300

    react app created

--- src/main/java/com/polianachagas/fullstack_backend/model/User.java
+++ src/main/java/com/polianachagas/fullstack_backend/model/User.java
@@ -13,27 +13,35 @@ public class User {
 	private String username;
 	private String name;
 	private String email;
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
