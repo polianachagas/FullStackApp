7d9f517d6d6329647afa4f50fc3a8644e57d9165
Poliana Chagas <polianarc934@gmail.com>
2024-08-06 11:57:33 -0300

    handling user exception

---
+++ src/main/java/com/polianachagas/fullstack_backend/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.polianachagas.fullstack_backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(Long id) {
+		super("Could not find the user with id " + id);
+	}
+}
