94492f2c143539e6ce04be0cc0b4e0711c69214b
Poliana Chagas <polianarc934@gmail.com>
2024-08-13 23:52:09 -0300

    view information for specific user

--- fullstack-front/src/layout/Navbar.js
+++ fullstack-front/src/layout/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
   return (
     <div>
         
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Full Stack Application</a>
+                <Link className="navbar-brand" to={"/"}>Full Stack Application</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
